e0ba1da7f94d3a46939f5c34734c6017b6031ceb
vkray <vitalikkrainyuk@yandex.ru>
2023-05-09 10:40:58 +0300

    hw1. Объекты и классы. Задача.

---
+++ src/Author.java
@@ -0,0 +1,17 @@
+class Author {                       // Класс автора
+    private String firstName;
+    private String lastName;
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
